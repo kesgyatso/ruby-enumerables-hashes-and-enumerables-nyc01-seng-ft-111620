fc0d7aab6b33c350a88da2a1aff22f824dcd8646
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-11-11 06:08:30 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,12 +1,4 @@
- passengers = {
-   suite_a: "Amanda Presley", 
-   suite_b: "Seymour Hoffman", 
-   suite_c: "Alfred Tennyson", 
-   suite_d: "Charlie Chaplin", 
-   suite_e: "Crumpet the Elf"
-   }
 
-winner = ""
 
 def select_winner(passengers)
   # add the code snippet here!
