879e3be500d22a1f93009fcc679ff595ce0cc778
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-11-11 06:09:25 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,4 +1,10 @@
-
+# passengers = {
+# suite_a: "Amanda Presley", 
+# suite_b: "Seymour Hoffman", 
+# suite_c: "Alfred Tennyson", 
+# suite_d: "Charlie Chaplin", 
+# suite_e: "Crumpet the Elf"
+# }
 
 def select_winner(passengers)
   # add the code snippet here!
@@ -13,3 +19,17 @@ passengers.each do |suite, name|
 end
 
 winner
+
+
+
+
+def select_winner(passengers)
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+
+  winner 
+end
